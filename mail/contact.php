407ae04dcf7d66cc06f3c4fa57cb9b55b9da9297
Jaspreet Singh Kohli <70095662+Jaspreetkohli72@users.noreply.github.com>
2021-10-22 18:48:02 +0530

    mail server change

--- mail/contact.php
+++ mail/contact.php
@@ -1,20 +1,47 @@
-<?php
-if(empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-  http_response_code(500);
-  exit();
+ <?php
+// if(empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+//   http_response_code(500);
+//   exit();
+// }
+
+// $name = strip_tags(htmlspecialchars($_POST['name']));
+// $email = strip_tags(htmlspecialchars($_POST['email']));
+// $m_subject = strip_tags(htmlspecialchars($_POST['subject']));
+// $message = strip_tags(htmlspecialchars($_POST['message']));
+
+// $to = "jaspreetkohli710@gmail.com"; // Change this email to your //
+// $subject = "$m_subject:  $name";
+// $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
+// $header = "From: $email";
+// $header .= "Reply-To: $email";	
+
+// if(!mail($to, $subject, $body, $header))
+//   http_response_code(500);
+$curl = curl_init();
+curl_setopt_array($curl, array(
+    CURLOPT_URL => $_ENV['TRUSTIFI_URL'] . "/api/i/v1/email",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "POST",
+    CURLOPT_POSTFIELDS =>"{\"recipients\":[{\"email\":\"jaspreetkohli@heroku.trustifi.com\"}],\"title\":\"Title\",\"html\":\"Body\"}",
+    CURLOPT_HTTPHEADER => array(
+        "x-trustifi-key: " . $_ENV['TRUSTIFI_KEY'],
+        "x-trustifi-secret: " . $_ENV['TRUSTIFI_SECRET'],
+        "content-type: application/json"
+    )
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+curl_close($curl);
+if ($err) {
+    echo "cURL Error #:" . $err;
+} else {
+    echo $response;
 }
 
-$name = strip_tags(htmlspecialchars($_POST['name']));
-$email = strip_tags(htmlspecialchars($_POST['email']));
-$m_subject = strip_tags(htmlspecialchars($_POST['subject']));
-$message = strip_tags(htmlspecialchars($_POST['message']));
-
-$to = "jaspreetkohli710@gmail.com"; // Change this email to your //
-$subject = "$m_subject:  $name";
-$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
-$header = "From: $email";
-$header .= "Reply-To: $email";	
-
-if(!mail($to, $subject, $body, $header))
-  http_response_code(500);
-?>
+?> 
